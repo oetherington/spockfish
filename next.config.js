8d301570c576634046e5943f65708eea2f8925dc
Ollie Etherington <ollie@etherington.xyz>
2021-12-10 14:42:47 +0000

    Fix production build webpack error
    The issues seems to have been caused by the size of the engine
    webworker - see https://github.com/vercel/next.js/issues/25484

--- next.config.js
+++ next.config.js
@@ -9,6 +9,29 @@ module.exports = {
 				name: 'static/media/[name].[hash:8].[ext]',
 			},
 		});
+
+		config.optimization.splitChunks = {
+			chunks: 'async',
+			minSize: 20000,
+			minRemainingSize: 0,
+			minChunks: 1,
+			maxAsyncRequests: 30,
+			maxInitialRequests: 30,
+			enforceSizeThreshold: 50000,
+			cacheGroups: {
+				defaultVendors: {
+					test: /[\\/]node_modules[\\/]/,
+						priority: -10,
+						reuseExistingChunk: true,
+					},
+				default: {
+					minChunks: 2,
+					priority: -20,
+					reuseExistingChunk: true,
+				},
+			},
+		};
+
 		return config;
 	},
 }
