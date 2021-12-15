4debd25bd3b547e7511524a776145713e4be5b57
Ollie Etherington <ollie@etherington.xyz>
2021-12-15 20:07:34 +0000

    Tidy up home page sizing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import type { NextPage } from 'next';
 import Link from 'next/link';
+import Flex from '@react-css/flex';
 import Layout from '~/components/Layout';
 import NewGame from '~/components/NewGame';
 import SpaceHero from '~/components/SpaceHero';
@@ -8,10 +9,14 @@ import SpaceHero from '~/components/SpaceHero';
 const Home: NextPage = () => {
 	return (
 		<Layout>
-			<div className='spacer'/>
-			<h1 className='trek-headline'>Star Trek Tri-Chess</h1>
-			<NewGame />
-			<div className='spacer'/>
+			<Flex
+				column
+				justifyContent='center'
+				className='full-height-minus-nav'
+			>
+				<h1 className='trek-headline'>Star Trek Tri-Chess</h1>
+				<NewGame />
+			</Flex>
 			<SpaceHero />
 		</Layout>
 	);
